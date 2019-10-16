2b6fb781ebc965494d03fc862f6147cae496c311
carlos rbson <carlosrobson0@gmail.com>
2019-10-15 22:06:37 -0300

    Route for materials

--- src/Routes.js
+++ src/Routes.js
@@ -1,1 +1,10 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
+import User from 'scenes/User/user';
+import MaterialReceived from 'scenes/MaterialReceived/MaterialReceived';
+
+export  default Routes = () => (
+    <Switch>
+        <Route exact path='/coopertativa' component={User}/>
+        <Route path='/material-in' component={MaterialReceived}/>
+    </Switch>
+);
--- src/scenes/MaterialReceived/MaterialReceived.js
+++ src/scenes/MaterialReceived/MaterialReceived.js
@@ -1,8 +1,29 @@
+import {HeaderPage, ContentPage,  Form, FormItem} from 'components/baseComponents';
 import {Layout} from 'antd';
 const { Content } = Layout;
 
 export default function MaterialReceived() {
   return (
-    <div></div>
+    <Content>
+      <HeaderPage>
+        Material Recebido
+      </HeaderPage>
+      <ContentPage>
+            <Form>
+                <FormItem label="Quantidade de Material">
+                    <Input placeholder="Quanto material foi recebido?" />
+                </FormItem>
+                <FormItem label="Entregador">
+                  <Input placeholder="Quem entregou o material? " />
+                </FormItem>
+                <FormItem label="Origem do Material">
+                  <Input placeholder="Domiciliar, Industrial ou comércio? " />
+                </FormItem>
+                <FormItem label="Data">
+                  <Input placeholder="Relativo a um dia, mês ou ano " />
+                </FormItem>
+            </Form>
+        </ContentPage>
+    </Content>
   );
 }
